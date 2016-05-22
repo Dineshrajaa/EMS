ca1365152b9f2afe0ddfcf48ca86d8a971c1e139
Dineshrajaa <dineshrajaa.93@gmail.com>
2016-05-22 18:40:21 +0530

    Included remember settings

--- www/js/settings.js
+++ www/js/settings.js
@@ -3,6 +3,9 @@ $("#statusCheck").change(function() {
         $("#statusBlock").show();
     } else {
         $("#statusBlock").hide();
+        $("#txtTime").val("");
+        $("#frequencySelect").val(0).selectmenu().selectmenu("refresh");
+        localStorage.savedLNPref="";
     }
 });
 
@@ -17,6 +20,10 @@ function saveLNPref() {
     }
 
     var  notificationTime = new Date($("#txtTime").val());
+    var notiSetObj={};
+    notiSetObj.notificationTime=$("#txtTime").val();
+    notiSetObj.frequency=$("#frequencySelect").val();
+    localStorage.savedLNPref=JSON.stringify(notiSetObj);
 
     //var sound = device.platform == 'Android' ? 'file://sound.mp3' : 'file://beep.caf';
 
@@ -25,7 +32,17 @@ function saveLNPref() {
         title: 'Dowells',
         text: 'Do you like to update your profile status?',
         at: notificationTime,
-        every:$("#frequencySelect").val(),
-        badge: 12
+        every:$("#frequencySelect").val()
     });
+}
+
+
+function restoreLNPref(){
+    // To restore the saved local notification preference
+    if(localStorage.savedLNPref!=undefined&&localStorage.savedLNPref!=""){
+        var savedLNPref=JSON.parse(localStorage.savedLNPref);
+        $("#statusCheck").attr("checked",true).flipswitch().flipswitch("refresh");
+        $("#txtTime").val(savedLNPref.notificationTime);
+        $("#frequencySelect").val(savedLNPref.frequency).selectmenu().selectmenu("refresh");
+    }
 }

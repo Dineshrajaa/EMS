a6232c7f2be1dc3a034a95573b495abbaf2ffa58
Dineshrajaa <dineshrajaa.93@gmail.com>
2016-09-09 04:13:55 +0530

    Changed dev url

--- www/js/common.js
+++ www/js/common.js
@@ -1,7 +1,7 @@
 // var serviceUrl = 'http://52.62.179.135/emsapi/api/'; // Production
 // var serviceUrl = 'http://202.60.69.12/emsapi/api/'; // Dev
-localStorage.serviceUrl=localStorage.serviceUrl||'http://52.62.179.135/emsapi/api/'; // Production
-// localStorage.serviceUrl=localStorage.serviceUrl||'http://202.60.69.12/emsapi/api/'; //dev 
+// localStorage.serviceUrl=localStorage.serviceUrl||'http://52.62.179.135/emsapi/api/'; // Production
+localStorage.serviceUrl=localStorage.serviceUrl||'http://52.63.234.33/emsapi/'; //dev 
 var serviceUrl=localStorage.serviceUrl; 
 $(document).ajaxStart(function() {
     showWait();
